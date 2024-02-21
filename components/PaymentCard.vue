0366d4be392d56673645226bcc58e80241539778
Imanol <imanol@meowtel.com>
2024-02-20 22:45:40 -0300

    new card style and history progess

--- components/PaymentCard.vue
+++ components/PaymentCard.vue
@@ -1,45 +1,39 @@
 <template>
-    <div v-if="!isLoading" class="border-2 border-solid border-white p-0 border-opacity-35 rounded-md mb-4 overflow-hidden">
-        <div v-if="isPaid && !edit" class="px-2 py-1 text-xs font-bold text-white shadow-md border-white" style="background-color: #4caf50;">
-            Paid
+    <div v-if="!isLoading" class="flex py-2 items-center card-container">
+        <div class="basis-1/6 flex items-center justify-start">
+            <div 
+                class="leading-tight mb-1 py-1 flex w-12 me-2 bg-opacity-15 flex-col items-center rounded-lg bg-white"
+                :class="{
+                    'bg-green-500': isPaid && !edit, 'bg-opacity-70': (isPaid || delayed) && !edit ,
+                    'bg-rose-500': !isPaid && delayed && !edit
+                }"
+            >
+                <span class="text-lg font-bold leading-none">{{day}}</span>
+                <span>{{weekDay}}</span>
+            </div>
         </div>
-        <div v-else-if="delayed && !edit" class="px-2 py-1 text-xs font-bold text-white shadow-md border-white" style="background-color: #af4c4c;">
-            Delayed
-        </div>
-        <div class="flex items-center justify-between max-h-40 overflow-hidden p-2 md:p-5 ">
-            <div class="w-1/3 me-4 flex flex-col justify-center items-center">
-                <span class="text-center">{{ month }} <strong class="text-2xl">{{ day }}</strong></span>
-                <svg style="max-width: 4rem;" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-                    <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
-                    <g id="SVGRepo_iconCarrier">
-                        <rect x="3" y="6" width="18" height="13" rx="2" stroke="#ffffff" stroke-width="2"
-                            stroke-linecap="round" stroke-linejoin="round"></rect>
-                        <path d="M3 10H20.5" stroke="#ffffff" stroke-width="2" stroke-linecap="round"
-                            stroke-linejoin="round"></path>
-                        <path d="M7 15H9" stroke="#ffffff" stroke-width="2" stroke-linecap="round"
-                            stroke-linejoin="round"></path>
-                    </g>
-                </svg>
-                <span class="text-center"><strong>{{ formatPrice(amount) }}</strong></span>
+        <div class="flex basis-full items-center border-b pb-2 self-end border-opacity-35 border-white">
+            <div class="basis-1/4 font-bold" style="min-width: 95px;">{{ formatPrice(amount) }}</div>
+            <div class="basis-full flex flex-col">
+                <div class="font-bold text-sm">{{ title }}</div>
+                <div class="text-sm">{{ description }}</div>
             </div>
-    
-            <div class="w-full self-start mt-3">
-                <span class="font-bold text-lg">{{ title }}</span>
-                <p class="mt-2 text-sm max-h-16 overflow-y-auto scrollbar-hide">{{ description }}</p>
-            </div>
-            <div v-if="!edit">
-                <button v-if="!isPaid" class="w-16 m-2" @click="markAsPaid(true)">Mark Paid</button>
-                <button v-else class="w-16 m-2 hover:bg-red-300" @click="markAsPaid(false)">Unpaid</button>
-            </div>
-            <div v-else>
-                <NuxtLink :to="`/edit/${id}`" class="w-16 m-2 as-button block text-center">Edit</NuxtLink>
-                <button class="w-16 m-2 bg-red-300" @click="removePay()">Remove</button>
+            <div class="hover-show">
+                <div v-if="!edit" class="m-1">
+                    <button v-if="!isPaid" class="w-20 leading-none my-0 h-auto" @click="markAsPaid(true)">Mark Paid</button>
+                    <button v-else class="w-20 leading-none my-0 h-auto hover:bg-red-300" @click="markAsPaid(false)">Unpaid</button>
+                </div>
+                <div v-else  class="m-1">
+                    <NuxtLink :to="`/edit/${id}`" class="w-16 leading-none my-0 inline-block as-button text-center" style="height: auto;">
+                        Edit
+                    </NuxtLink>
+                    <button class="w-16 leading-none my-0 h-auto bg-red-300 border-red-300" @click="removePay()">Remove</button>
+                </div>
             </div>
         </div>
     </div>
-    <div v-else class="border-2 border-solid border-white p-0 border-opacity-35 rounded-md mb-4 overflow-hidden min-h-36 flex justify-center items-center">
-        <Loader/>
+    <div v-else>
+        <Loader />
     </div>
     <ConfirmDialogue ref="confirmDialogue" />
 </template>
@@ -47,7 +41,7 @@
 <script setup>
 
 const props = defineProps({
-    description: {  
+    description: {
         required: false,
         default: "",
         type: String
@@ -89,16 +83,17 @@ const dueDateObject = $dayjs(props.dueDate, { format: 'MM/DD/YYYY' });
 const month = ref(dueDateObject.format('MMM'));
 const day = ref(dueDateObject.format('DD'));
 const delayed = ref(dueDateObject.isBefore($dayjs(), 'day'))
+const weekDay = ref(dueDateObject.format('ddd'))
 const isLoading = ref(false)
 
 // ----- Define methods ------------
 async function markAsPaid(value) {
     // Confirm dialogue
-    const confirmed = await confirmDialogue.value.openDialog({edit: true});
+    const confirmed = await confirmDialogue.value.openDialog({ edit: true });
 
-    if(!confirmed) {
+    if (!confirmed) {
         return;
-    } 
+    }
 
     isLoading.value = true;
     const result = await indexStore.editIsPaid(props.id, value);
@@ -107,7 +102,7 @@ async function markAsPaid(value) {
         type: "success",
         message: "Marked as paid successfully"
     };
-    if(!result) {
+    if (!result) {
         toastMessage.type = "error";
         toastMessage.message = "Something went wrong, please try again or contact the support team.";
     }
@@ -120,19 +115,31 @@ async function removePay() {
     // Confirm dialogue
     const confirmed = await confirmDialogue.value.openDialog();
 
-    if(!confirmed) {
+    if (!confirmed) {
         return;
-    } 
+    }
 
     const removed = indexStore.removePayment(props.id);
     const toastMessage = {
         type: "success",
         message: "Payment removed successfully"
     };
-    if(!removed) {
+    if (!removed) {
         toastMessage.type = "error";
         toastMessage.message = "Something went wrong, please try again or contact the support team.";
     }
     useToast(toastMessage.type, toastMessage.message);
 }
 </script>
+
+<style>
+.hover-show {
+    display: none;
+    opacity: 0;
+}
+.card-container:hover .hover-show {
+    display: block;
+    opacity: 1;
+    transition: all .3 ease;
+}
+</style>
--- pages/history.vue
+++ pages/history.vue
@@ -3,8 +3,8 @@
         <h2>Payments' History</h2>
         <div>
             <div v-for="(tracker, index) in formattedHistory" :key="index">
-                <div>
-                    <button @click="toggleShow"
+                <div class="hover-expand">
+                    <button @click="toggleShow(tracker.id)"
                         class="px-0 text-xl no-button my-0 mt-2 flex items-center justify-between w-full">
                         <span class="w-32 flex-initial text-start">{{ tracker.month }} {{ tracker.year }}</span>
                         <div class="w-full border-b-2 border-solid border-white border-opacity-35"></div>
@@ -12,19 +12,22 @@
                 </div>
                 <div class="overflow-hidden">
                     <Transition>
-                        <div v-if="show">
-                            <div class="flex py-2 items-center" v-for="(pay, idxPay) in tracker" :key="`pay-${index}-${idxPay}`">
+                        <div v-if="show[tracker.id]">
+                            <div class="flex py-2 items-center" v-for="(pay, idxPay) in tracker.payments" :key="`pay-${index}-${idxPay}`">
                                 <div class="basis-1/6 flex items-center justify-start">
-                                    <div class="leading-tight mb-1 py-1 flex w-12 me-2 bg-white bg-opacity-35 flex-col items-center rounded-full">
-                                        <span>Sat</span>
-                                        <span class="text-lg font-bold leading-none">05</span>
+                                    <div 
+                                        class="leading-tight mb-1 py-1 flex w-12 me-2 bg-opacity-15 flex-col items-center rounded-lg"
+                                        :class="{'bg-green-500': pay.isPaid, 'bg-opacity-70': pay.isPaid, 'bg-white': !pay.isPaid }"
+                                    >
+                                        <span class="text-lg font-bold leading-none">{{pay.day}}</span>
+                                        <span>{{pay.weekDay}}</span>
                                     </div>
                                 </div>
                                 <div class="flex basis-full items-center border-b pb-2 self-end border-opacity-35 border-white">
-                                    <div class="basis-1/4 font-bold">Amount</div>
+                                    <div class="basis-1/4 font-bold" style="min-width: 95px;">{{ formatPrice(pay.amount) }}</div>
                                     <div class="basis-full flex flex-col">
-                                        <div class="font-bold text-sm">Title</div>
-                                        <div class="text-sm">Description</div>
+                                        <div class="font-bold text-sm">{{ pay.title }}</div>
+                                        <div class="text-sm">{{ pay.description }}</div>
                                     </div>
                                 </div>
                             </div>
@@ -41,7 +44,7 @@
 const { $dayjs } = useNuxtApp()
 
 // ----- Define Vars -------------
-const show = ref(true);
+const show = ref({});
 const formattedHistory = ref([])
 
 // ----- Define Pinia Vars
@@ -50,13 +53,16 @@ const indexStore = useIndexStore()
 await indexStore.loadHistory();
 // Retrieve values
 const { getHistory: history } = storeToRefs(indexStore);
-
 processHistory();
 
-console.log(formattedHistory.value)
-
 function processHistory() {
+    // Flag to set show object with first element opened
+    let isFirst = true;
     formattedHistory.value = history.value.map(el => {
+        // Set show object
+        show.value[el.id] = isFirst;
+        isFirst = false;
+
         const auxObj = {...el};
         const date = $dayjs.unix(el.createdAt.seconds);
 
@@ -66,12 +72,25 @@ function processHistory() {
         auxObj.weekDay =  date.format('ddd')
         auxObj.year =  date.format('YYYY')
 
+        // Format payments date too
+        auxObj.payments = auxObj.payments.map(payment => {
+            const auxPayObj = {...payment};
+            const payDate = $dayjs(payment.dueDate, { format: 'MM/DD/YYYY' });
+            // Format legible date
+            auxPayObj.day =  payDate.format('D')
+            auxPayObj.month =  payDate.format('MMM')
+            auxPayObj.weekDay =  payDate.format('ddd')
+            auxPayObj.year =  payDate.format('YYYY')
+
+            return auxPayObj;
+        })
+
         return auxObj;
     })
 }
 
-function toggleShow() {
-    show.value = !show.value;
+function toggleShow(id) {
+    show.value[id] = !show.value[id];
 }
 
 useHead({
@@ -96,4 +115,11 @@ useHead({
 .v-leave-to {
     transform: translateY(-3rem);
     opacity: 0;
-}</style>
+}
+
+.hover-expand:hover {
+    transform: scale(1.01);
+    transition: all .2s ease-out;
+}
+
+</style>
